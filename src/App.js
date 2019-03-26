9e2d82bcec70eaa766924676b1b5d9217b94fa89
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-26 16:41:45 -0400

    changed radio buttons to cropdowns

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ class App extends Component {
   state = {
     characters: [],
     races: [],
-    characterNameInput: ''
+    characterNameInput: '',
+    weaponInput: '',
+    professionInput: '',
+    residenceInput: '',
+    ringInput: false,
+    raceSelectInput: '',
+    raceNameInput: '',
+    languageInput: '',
+    immortalInput: false
   }
 
   componentDidMount() {
@@ -57,6 +65,42 @@ class App extends Component {
     })
   }
 
+  ChangeWeapon = event => {
+    this.setState({
+      weaponInput: event.target.value
+    })
+  }
+
+  ChangeProfession = event => {
+    this.setState({
+      professionInput: event.target.value
+    })
+  }
+
+  ChangeResidence = event => {
+    this.setState({
+      residenceInput: event.target.value
+    })
+  }
+
+  ChangeRaceSelect = event => {
+    this.setState({
+      raceSelectInput: event.target.value
+    })
+  }
+
+  ChangeRaceName = event => {
+    this.setState({
+      raceNameInput: event.target.value
+    })
+  }
+
+  ChangeLanguage = event => {
+    this.setState({
+      languageInput: event.target.value
+    })
+  }
+
   render() {
     return (
       <>
@@ -74,32 +118,38 @@ class App extends Component {
           placeholder="Character Name"
           onChange={this.ChangeCharacterName}
         />
-        <input className="text-field" placeholder="Weapon of Choice" />
-        <input className="text-field" placeholder="Profession" />
-        <input className="text-field" placeholder="Residence" />
-        <p>Have they wielded the One Ring?</p>
-        <section className="radio-group">
-          <input type="radio" id="yes" name="option" value="yes" />
-          <label for="yes">Yes</label>
-          <input type="radio" id="no" name="option" value="no" />
-          <label for="no">No</label>
-        </section>
-        <section className="character-controls">
-          <select className="drop-down">
-            <option value="Select Race">Select Race</option>
-            {this.state.races.map(race => {
-              return <option value={race.raceId}>{race.raceName}</option>
-            })}
-          </select>
-          <section className="button-group">
-            <button onClick={this.AddCharacter}>Add Character</button>
-            <button onClick={this.UpdateCharacter}>Update Character</button>
-            <button
-              onClick={this.DeleteCharacter(this.state.characterNameInput)}
-            >
-              Delete Character
-            </button>
-          </section>
+        <input
+          className="text-field"
+          placeholder="Weapon of Choice"
+          onChange={this.ChangeWeapon}
+        />
+        <input
+          className="text-field"
+          placeholder="Profession"
+          onChange={this.ChangeProfession}
+        />
+        <input
+          className="text-field"
+          placeholder="Residence"
+          onChange={this.ChangeResidence}
+        />
+        <select className="one-ring">
+          <option value="default">Wielded the One Ring?</option>
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
+        <select className="drop-down" onChange={this.ChangeRaceSelect}>
+          <option value="default">Select Race</option>
+          {this.state.races.map(race => {
+            return <option value={race.raceId}>{race.raceName}</option>
+          })}
+        </select>
+        <section className="button-group">
+          <button onClick={this.AddCharacter}>Add Character</button>
+          <button onClick={this.UpdateCharacter}>Update Character</button>
+          <button onClick={this.DeleteCharacter(this.state.characterNameInput)}>
+            Delete Character
+          </button>
         </section>
         <h2>Races</h2>
         <ul className="race-list">
@@ -107,15 +157,21 @@ class App extends Component {
             return <li key={race.id}>{race.raceName}</li>
           })}
         </ul>
-        <input className="text-field" placeholder="Race" />
-        <input className="text-field" placeholder="Native Language" />
-        <p>Are they immortal unless slain?</p>
-        <section className="radio-group">
-          <input type="radio" id="yes" name="option" value="yes" />
-          <label for="yes">Yes</label>
-          <input type="radio" id="no" name="option" value="no" />
-          <label for="no">No</label>
-        </section>
+        <input
+          className="text-field"
+          placeholder="Race"
+          onChange={this.ChangeRaceName}
+        />
+        <input
+          className="text-field"
+          placeholder="Native Language"
+          onChange={this.ChangeLanguage}
+        />
+        <select className="is-immortal">
+          <option value="default">Immortal unless slain?</option>
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
         <section className="button-group">
           <button onClick={this.AddRace}>Add Race</button>
           <button onClick={this.UpdateRace}>Update Race</button>
